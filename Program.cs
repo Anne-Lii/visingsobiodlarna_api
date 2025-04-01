3209727d062d23f3ee1b4f7475248803563f063c
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-01 11:46:42 +0200

    konfigurerat JWT-hantering och lagt till inställningar i appsetting.json

--- Program.cs
+++ Program.cs
@@ -2,14 +2,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using visingsobiodlarna_backend.Models;
 using visingsobiodlarna_backend.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -18,6 +17,31 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectio
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
+
+
+//Registrerar autentisering och anger JWT som standard
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+//Lägger till stöd för JWT-baserad autentisering
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,//Kontroll att token är utfärdad av rätt källa
+        ValidateAudience = true,//Kontroll att token är avsedd för rätt mottagare
+        ValidateLifetime = true, //Kontroll att token inte har gått ut
+        ValidateIssuerSigningKey = true,//Kontroll att token är korrekt validerad
+
+        //hämtar inställningar från appsettings.json
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))//Hemlig nyckel som används för att validera token
+    };
+});
 
 var app = builder.Build();
 
@@ -28,9 +52,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
