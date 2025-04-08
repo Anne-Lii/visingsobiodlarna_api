4c511d996ef3d8e0a22cefe2864593fb9adab294
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-08 11:34:41 +0200

    rättat till rollhantering för JWT och authorize

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using visingsobiodlarna_backend.Services;
+using System.Security.Claims;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -43,7 +44,8 @@ builder.Services.AddAuthentication(options =>
         //hämtar inställningar från appsettings.json
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))//Hemlig nyckel som används för att validera token
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),//Hemlig nyckel som används för att validera token
+        RoleClaimType = ClaimTypes.Role
     };
 });
 
