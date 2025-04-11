1b7a4ec0a85058cc2f774ef4eb1702f93574d52e
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-11 15:20:02 +0200

    liten justering på kommentar

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -93,7 +93,7 @@ public class AuthController : ControllerBase
             new Claim(ClaimTypes.NameIdentifier, user.Id)
         };
 
-        // Lägger till varje roll som en egen claim i token
+        // Lägger till varje roll som en egen claimi token
         claims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
