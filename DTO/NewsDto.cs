9ac9cae66794028eb99d0056afb810e142619a00
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-24 15:55:49 +0200

    skapat DTO för news

---
+++ DTO/NewsDto.cs
@@ -0,0 +1,10 @@
+namespace visingsobiodlarna_backend.DTOs
+{
+    public class NewsDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? PublishDate { get; set; }
+    }
+}
