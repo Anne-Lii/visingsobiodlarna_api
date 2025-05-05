294761884d48cf9007a898fc3e2b15074ba2f85c
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-05-05 13:52:27 +0200

    lagt till jsonPropertyName för att matcha frontends camelCase

--- DTO/CreateApiaryDto.cs
+++ DTO/CreateApiaryDto.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace visingsobiodlarna_backend.DTOs;
 
 public class CreateApiaryDto
 {
+    [JsonPropertyName("name")] //Matchar frontendens camelCase
     public string Name { get; set; } = null!;
+    
+    [JsonPropertyName("location")]
     public string Location { get; set; } = null!;
 }
--- DTO/GetApiaryDto.cs
+++ DTO/GetApiaryDto.cs
@@ -1,8 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace visingsobiodlarna_backend.DTOs;
 
 public class GetApiaryDto
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+
+    [JsonPropertyName("name")]
     public string Name { get; set; } = null!;
+
+    [JsonPropertyName("location")]
     public string Location { get; set; } = null!;
 }
