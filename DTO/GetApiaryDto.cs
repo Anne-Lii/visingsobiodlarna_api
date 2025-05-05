53467aa43ed29bc89f82b661a64e55716d0b2be1
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-05-05 12:46:54 +0200

    lagt till DTOs och endpoint för att hämta inloggad användares bigårdar

---
+++ DTO/GetApiaryDto.cs
@@ -0,0 +1,8 @@
+namespace visingsobiodlarna_backend.DTOs;
+
+public class GetApiaryDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Location { get; set; } = null!;
+}
